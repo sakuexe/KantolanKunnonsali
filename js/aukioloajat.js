83d22c6da703a9c8acd23c6fb08a922229118bd4
sakuexe <saku.karttunen@student.hamk.fi>
2022-07-27 10:44:36 +0300

    asiakkaan toivomat muokkaukset tehtynä

--- js/aukioloajat.js
+++ js/aukioloajat.js
@@ -6,13 +6,14 @@ fetch("../aukioloajat.json")
         let aukioloajat = document.querySelectorAll(".aukioloaika")
 
         /* 
-            Aukioloajat -muuttuja sisältää sivun 8 elementtiä: "Ma", "Ti, "Ke", "To", "Pe", "Lauantai", "Sunnuntain" ja "Avainkortilla" aiktaulutekstit
+            Aukioloajat -muuttuja sisältää sivun 8 elementtiä: "Maanantai", "Tiistai, "Keskiviikko", "Torstai", "Perjantai", "Lauantai", "Sunnuntain" 
+            sekä "Avainkortilla" aikataulutekstit
             ---
-            aukioloajat[0] = Ma                 - index.html, rivi 99
-            aukioloajat[1] = Ti                 - index.html, rivi 107
-            aukioloajat[2] = Ke                 - index.html, rivi 115
-            aukioloajat[3] = To                 - index.html, rivi 123
-            aukioloajat[4] = Pe                 - index.html, rivi 131
+            aukioloajat[0] = Maanantai          - index.html, rivi 99
+            aukioloajat[1] = Tiistai            - index.html, rivi 107
+            aukioloajat[2] = Keskiviikko        - index.html, rivi 115
+            aukioloajat[3] = Torstai            - index.html, rivi 123
+            aukioloajat[4] = Perjantai          - index.html, rivi 131
             aukioloajat[5] = Lauantai           - index.html, rivi 139
             aukioloajat[6] = Sunnuntai          - index.html, rivi 146
             aukioloajat[7] = Avainkortilla      - index.html, rivi 150
@@ -22,11 +23,11 @@ fetch("../aukioloajat.json")
             ja tämän avulla voimme muuttaa elementin sisäistä tekstiä json dataan.
         */
 
-        aukioloajat[0].innerHTML = jsondata.Ma
-        aukioloajat[1].innerHTML = jsondata.Ti
-        aukioloajat[2].innerHTML = jsondata.Ke
-        aukioloajat[3].innerHTML = jsondata.To
-        aukioloajat[4].innerHTML = jsondata.Pe
+        aukioloajat[0].innerHTML = jsondata.Maanantai
+        aukioloajat[1].innerHTML = jsondata.Tiistai
+        aukioloajat[2].innerHTML = jsondata.Keskiviikko
+        aukioloajat[3].innerHTML = jsondata.Torstai
+        aukioloajat[4].innerHTML = jsondata.Perjantai
         aukioloajat[5].innerHTML = jsondata.Lauantai
         aukioloajat[6].innerHTML = jsondata.Sunnuntai
         aukioloajat[7].innerHTML = jsondata.Avainkortilla
